ef031e0b795948b532e8f8bb51bc84f1d37110b3
Imran-tec <imranhossainbd508@gmail.com>
2020-10-03 16:14:53 +0600

    my-server

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const port = 5000
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./burj-al-arab-aa1e1-firebase-adminsdk-u33op-68ff6c21c8.json");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://burj-al-arab-aa1e1.firebaseio.com"
+});
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+
+
+
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+
+
+
+
+const uri = "mongodb+srv://arabianData:ik636851@cluster0.0jhuw.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+client.connect(err => {
+  const collection = client.db("burjAlArab").collection("booking");
+console.log('db connection success')
+  
+app.post('/addBooking', (req,res) => {
+    const newBooking = req.body;
+  collection.insertOne(newBooking)
+  .then(result => {
+      res.send(result.insertedCount > 0) 
+  })
+});
+
+app.get('/booking', (req,res) => {
+const bearer = req.headers.authorization;
+    if(bearer && bearer.startsWith('Bearer')){
+        const idToken = bearer.split(' ')[1];
+        console.log({idToken})
+        admin.auth().verifyIdToken(idToken)
+        .then(function(decodedToken) {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          
+          if(tokenEmail == queryEmail){
+            collection.find({email: queryEmail})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+          }
+       
+        }).catch(function(error) {
+          // Handle error
+        });
+    }
+    else{
+      res.status().res.send('unauthorization')
+    }
+    
+   
+
+
+   
+   
+})
+
+
+});
+
+
+
+
+
+app.listen(port,() => {
+    console.log('server start')
+})
